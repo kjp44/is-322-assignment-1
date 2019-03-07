f98d34085af7c147c5f0ffe1cf6910bd905d55ec
Kyle Payne <kjp44@njit.edu>
2019-03-07 18:28:43 -0500

    Fully functional carousel

--- js/landing.js
+++ js/landing.js
@@ -1,13 +1,56 @@
 var slideIndex = 0;
 var slides = document.getElementsByClassName("carousel-slide");
-showSlides();
+var indicators = document.getElementsByClassName("carousel-indicator");
+let interval = setInterval(showSlides, 4000);
+
+function nextSlide(){
+	slides[slideIndex].className = "carousel-slide";
+	indicators[slideIndex].className = "carousel-indicator";
+	slideIndex++;
+	if (slideIndex > slides.length - 1){
+		slideIndex = 0;
+	}
+	slides[slideIndex].className += " active";
+	indicators[slideIndex].className += " active";
+	clearInterval(interval);
+	interval = setInterval(showSlides, 4000);
+}
+
+function prevSlide(){
+	slides[slideIndex].className = "carousel-slide";
+	indicators[slideIndex].className = "carousel-indicator";
+	slideIndex--;
+	if (slideIndex < 0){
+		slideIndex = slides.length - 1;
+	}
+	slides[slideIndex].className += " active";
+	indicators[slideIndex].className += " active";
+	clearInterval(interval);
+	interval = setInterval(showSlides, 4000);
+}
+
+function setCurrentSlide(slide){
+	slideIndex = slide;
+	for(var i = 0; i < slides.length; i++){
+		slides[i].className = "carousel-slide"
+		indicators[i].className = 'carousel-indicator'
+	}
+	slides[slideIndex].className += " active";
+	indicators[slideIndex].className += " active"
+	clearInterval(interval);
+	interval = setInterval(showSlides, 4000);
+}
 
 function showSlides() {
-  slides[slideIndex].className = "carousel-slide";
-  slideIndex++;
-  if (slideIndex > slides.length - 1){
-    slideIndex = 0;
-  }
-  slides[slideIndex].className += " active";
-  setTimeout(showSlides, 2000);
+  	console.log(slideIndex)
+  	slideIndex++;
+  	for(var i = 0; i < slides.length; i++){
+		slides[i].className = "carousel-slide"
+		indicators[i].className = 'carousel-indicator'
+	}
+	if (slideIndex > slides.length - 1){
+		slideIndex = 0;
+	}
+  	slides[slideIndex].className += " active";
+  	indicators[slideIndex].className += " active"
 }
